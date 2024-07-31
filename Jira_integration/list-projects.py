753e5da99efd67a45fd27ff6633c6b09fe23342a
GouriS <gourisshinde21@gmail.com>
2024-07-31 19:13:06 +0530

    removed token

--- Jira_integration/list-projects.py
+++ Jira_integration/list-projects.py
@@ -5,8 +5,8 @@ from requests.auth import HTTPBasicAuth
 import json
 
 url = "https://gourishinde9021.atlassian.net/rest/api/3/project"
-API_TOKEN = "ATATT3xFfGF0GHkNOYO9K8jMsBamQv6-EapLfAY31KpSfik7xAmvQ4SuGxyN6uNK8z7UyWcg0tyWbrsXS82lLKyvFu5jfG8JKNknr4dakahhkDRVHFCuAqsgh9T1AL6hjjDg-pK0RCaBHd7sMP54pnNlDRvRCmuN8ZklwG0mXiIg9B5_TCUsooY=5CC161FF"
-auth = HTTPBasicAuth("gourishinde9021@gmail.com", API_TOKEN)
+API_TOKEN = "<api_token>"
+auth = HTTPBasicAuth("<email>", API_TOKEN)
 
 headers = {
   "Accept": "application/json"
