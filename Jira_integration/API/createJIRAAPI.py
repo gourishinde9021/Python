ae2ee1673eae7fdcd1635cd26050a8a93079a85a
GouriS <gourisshinde21@gmail.com>
2024-08-01 18:18:06 +0530

    updated URL

--- Jira_integration/API/createJIRAAPI.py
+++ Jira_integration/API/createJIRAAPI.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/createJIRA", methods = ['POST'])            # Decorator
 def createJIRA():
-    url = "https://gourishinde9021.atlassian.net/rest/api/3/project"
+    url = "https://gourishinde9021.atlassian.net/rest/api/3/issue"
     API_TOKEN = "ATATT3xFfGF0GHkNOYO9K8jMsBamQv6-EapLfAY31KpSfik7xAmvQ4SuGxyN6uNK8z7UyWcg0tyWbrsXS82lLKyvFu5jfG8JKNknr4dakahhkDRVHFCuAqsgh9T1AL6hjjDg-pK0RCaBHd7sMP54pnNlDRvRCmuN8ZklwG0mXiIg9B5_TCUsooY=5CC161FF"
     auth = HTTPBasicAuth("gourishinde9021@gmail.com", API_TOKEN)
 
